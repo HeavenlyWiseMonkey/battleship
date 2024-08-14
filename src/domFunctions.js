007212476610aeaf56cd35e69754e36df8a8cff0
odin <139291798+minecrafter021345@users.noreply.github.com>
2024-08-13 20:46:32 -0600

    Create basic dom functions showing ships and squares

--- src/domFunctions.js
+++ src/domFunctions.js
@@ -2,18 +2,30 @@ import Gameboard from './classes/Gameboard';
 import Player from './classes/Player';
 import Ship from './classes/Ship';
 
+// create squares for two boards
 function boardSetup() {
-    const board = document.querySelector('.board');
+    const boards = document.querySelectorAll('.board');
 
-    for (let i=0; i<100; i++) {
-        let square = document.createElement('div');
-        square.classList.add('square');
-        board.appendChild(square);
+    boards.forEach((board) => {
+        for (let i=0; i<100; i++) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            board.appendChild(square);
+        }
+    })
+}
+
+// make specific board get ship added
+function addShip(startX, endX, startY, endY, owner) {
+    const board = document.querySelector(`.${owner}`);
+    console.log(board);
+    const squares = board.children;
+    for (let i=startX; i<endX; i++) {
+        squares[i].classList.add('ship');
+    }
+    for (let i=startY; i<endY; i++) {
+        squares[i*10+startX].classList.add('ship');
     }
 }
 
-function addShip() {
-
-}
-
-export {boardSetup};
+export {boardSetup, addShip};
--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@ import * as css from "./index.css";
 import Gameboard from './classes/Gameboard';
 import Player from './classes/Player';
 import Ship from './classes/Ship';
-import {boardSetup} from './domFunctions.js';
+import {boardSetup, addShip} from './domFunctions.js';
+
+const destroyer = new Ship(2);
+const submarine = new Ship(3);
+const cruiser = new Ship(3);
+const battleship = new Ship(4);
+const carrier = new Ship(5);
+
+const player = new Player();
+const computer = new Player();
 
 boardSetup();
+
+player.gameboard.place(destroyer, 0, 2, 0, 0);
+addShip(0, 2, 0, 0, 'player');
+player.gameboard.place(submarine, 3, 3, 3, 6);
+addShip(3, 3, 3, 6, 'player');
+player.gameboard.place(cruiser, 5, 8, 0, 0);
+addShip(5, 8, 0, 0, 'player');
+player.gameboard.place(battleship, 2, 2, 0, 4,);
+addShip(2, 2, 0, 4, 'player');
+player.gameboard.place(carrier, 5, 5, 4, 9);
+addShip(5, 5, 4, 9, 'player');
+
+computer.gameboard.place(destroyer, 5, 7, 0, 0);
+addShip(5, 7, 0, 0, 'computer');
+
