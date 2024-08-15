a4235b25dee4a60ede11545b9b2e4264b5db30dd
odin <139291798+minecrafter021345@users.noreply.github.com>
2024-08-14 20:37:25 -0600

    Player now only attacks the computer board

--- src/domFunctions.js
+++ src/domFunctions.js
@@ -3,17 +3,23 @@ import Player from './classes/Player';
 import Ship from './classes/Ship';
 
 // create squares for two boards
-function boardSetup() {
-    const boards = document.querySelectorAll('.board');
+function boardSetup(player) {
+    const playerBoard = document.querySelector('.player');
+    const computerBoard = document.querySelector('.computer');
 
-    boards.forEach((board) => {
-        for (let i=0; i<100; i++) {
-            let square = document.createElement('div');
-            square.classList.add('square');
-            square.addEventListener('click', attack);
-            board.appendChild(square);
-        }
-    })
+    // setup player board
+    for (let j=0; j<100; j++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        playerBoard.appendChild(square);
+    }
+    // setup computer board
+    for (let j=0; j<100; j++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.addEventListener('click', () => attack(square, player));
+        computerBoard.appendChild(square);
+    }
 }
 
 // make specific board get ship added
@@ -29,16 +35,49 @@ function addShip(ship, player, startX, endX, startY, endY) {
     }
 }
 
-// function attack() {
-//     // get index of square and update colour
-//     const player = this.parentElement;
-//     const index = Array.from(player.children).indexOf(this);
-//     if (receiveAttack(Math.floor(index/10), index%10)) {
-//         this.classList.addEventListener('hit');
-//     }
-//     else {
-//         this.classList.addEventListener('miss');
-//     }
-// }
+function attack(self, player) {
+    // get index of square and update colour
+    const board = self.parentElement;
+    const index = Array.from(board.children).indexOf(self);
+    if (player.gameboard.receiveAttack(Math.floor(index/10), index%10)) {
+        self.classList.add('hit');
+    }
+    else {
+        self.classList.add('miss');
+    }
+}
 
-export {boardSetup, addShip};
+function changeTurn(self, player, computer) {
+    attack(self, player, computer);
+}
+
+function computerAttack() {
+
+}
+
+function game() {
+    const player = new Player('player');
+    const computer = new Player('computer');
+
+    boardSetup(computer);
+
+    const destroyer = new Ship(2);
+    const submarine = new Ship(3);
+    const cruiser = new Ship(3);
+    const battleship = new Ship(4);
+    const carrier = new Ship(5);
+
+    addShip(destroyer, player, 0, 2, 0, 0);
+    addShip(submarine, player, 3, 3, 3, 6);
+    addShip(cruiser, player, 5, 8, 0, 0);
+    addShip(battleship, player, 2, 2, 0, 4);
+    addShip(carrier, player, 5, 5, 4, 9);
+
+    addShip(destroyer, computer, 5, 7, 0, 0);
+    addShip(submarine, computer, 3, 3, 0, 3);
+    addShip(cruiser, computer, 0, 3, 4, 4);
+    addShip(battleship, computer, 1, 5, 7, 7);
+    addShip(carrier, computer, 5, 10, 9, 9);
+}
+
+export {game};
--- src/index.js
+++ src/index.js
@@ -3,28 +3,6 @@ import * as css from "./index.css";
 import Gameboard from './classes/Gameboard';
 import Player from './classes/Player';
 import Ship from './classes/Ship';
-import {boardSetup, addShip} from './domFunctions.js';
+import {game} from './domFunctions.js';
 
-const destroyer = new Ship(2);
-const submarine = new Ship(3);
-const cruiser = new Ship(3);
-const battleship = new Ship(4);
-const carrier = new Ship(5);
-
-const player = new Player('player');
-const computer = new Player('computer');
-
-boardSetup();
-
-addShip(destroyer, player, 0, 2, 0, 0);
-addShip(submarine, player, 3, 3, 3, 6);
-addShip(cruiser, player, 5, 8, 0, 0);
-addShip(battleship, player, 2, 2, 0, 4);
-addShip(carrier, player, 5, 5, 4, 9);
-
-addShip(destroyer, computer, 5, 7, 0, 0);
-addShip(submarine, computer, 3, 3, 0, 3);
-addShip(cruiser, computer, 0, 3, 4, 4);
-addShip(battleship, computer, 1, 5, 7, 7);
-addShip(carrier, computer, 5, 10, 9, 9);
-
+game();
